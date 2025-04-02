d2eb3e7d348dfb0bb3abebf0382cee9c02b82942
duque777 <duke_duquejr@hotmail.com>
2025-04-02 10:21:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.get('/summary', async (req, res) => {
     FROM questions
     WHERE email = $1
     GROUP BY question
-    Order BY times, correct desc
+    Order BY count(*) desc
   `, [currentUser]);
 
   res.json({ simulators, total, correct, incorrect, percent, questions: summary.rows });
